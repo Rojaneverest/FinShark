4ea0dac8b2f9ed88c4e83b28aeca6bbc5df0be7e
Rojan Thapa <rojaneverest@gmail.com>
2025-01-30 04:39:23 +0545

    fix: add apikey

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5fbc64f55f1acf2417259795ca6648e54295c81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9ae48c3ceaffc767a60d7325c3e4d64ac1539f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
